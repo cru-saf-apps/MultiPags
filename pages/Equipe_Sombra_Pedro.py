c46807e8f490e6d5fb57851415a18709889fd476
cru-saf-apps <116762584+cru-saf-apps@users.noreply.github.com>
2022-11-03 16:35:24 -0300

    Update Equipe_Sombra_Pedro.py

--- pages/Equipe_Sombra_Pedro.py
+++ pages/Equipe_Sombra_Pedro.py
@@ -50,6 +50,6 @@ while t<len(nomes):
     lista_results.append(nomes[t])
   t += 1
 
-st.write(base[base.Jogador.isin(lista_results)][['Jogador','Equipe atual','Idade']])
+st.write(base[base.Jogador.isin(lista_results)].reset_index(drop=True)[['Jogador','Equipe atual','Idade']][0])
 
 
