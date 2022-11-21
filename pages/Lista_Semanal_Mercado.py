8460e67c326c3f8a241904df43a6353f11c37674
cru-saf-apps <116762584+cru-saf-apps@users.noreply.github.com>
2022-11-21 13:57:04 -0300

    Update Lista_Semanal_Mercado.py

--- pages/Lista_Semanal_Mercado.py
+++ pages/Lista_Semanal_Mercado.py
@@ -4,4 +4,22 @@ import openpyxl
 
 base = pd.read_excel('DESTAQUES.xlsx',engine='openpyxl')
 
+dic_classe = {'A':6,
+              'B':5,
+              'C':4,
+              'D':3,
+              'E':2,
+              'F':1}
+
+base['ClasseNum'] = dic_classe[base['Classe']]
+
+base['Nota'] = ''
+
+
+
+posicoes = [1,2,3,4,5,6,7,8,8.5,9,9.5,10,11]
+
+
+
+
 st.write(base)
