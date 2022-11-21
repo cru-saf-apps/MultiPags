8c911bff5fe7d384c7e3a3a836e59727f799e2d2
cru-saf-apps <116762584+cru-saf-apps@users.noreply.github.com>
2022-11-21 17:09:07 -0300

    Update Historico_Mercado.py

--- pages/Historico_Mercado.py
+++ pages/Historico_Mercado.py
@@ -13,21 +13,20 @@ df_hist = pd.DataFrame()
 
 for jogador in negoc.ID:
   
-  aux_df = pd.DataFrame(columns = ['Atleta','Posição','Clube','Ano'])
+  dic_aux = {'Atleta':negoc[negoc.ID == jogador]['ATLETA'],
+             'Posição'negoc[negoc.ID == jogador]['POSIÇÃO'],
+             'Clube':negoc[negoc.ID == jogador]['CLUBE'],
+             'Ano':negoc[negoc.ID == jogador]['ANO']}
   
-  aux_df['Atleta'][0] = negoc[negoc.ID == jogador]['ATLETA']
-  aux_df['Posição'][0] = negoc[negoc.ID == jogador]['POSIÇÃO']
-  aux_df['Clube'][0] = negoc[negoc.ID == jogador]['CLUBE']
-  aux_df['Ano'][0] = negoc[negoc.ID == jogador]['ANO']
+  aux_df = pd.DataFrame(dic_aux)
   
   hist_jog = hist[hist['ID ATLETA'] == jogador].reset_index(drop=True)
   comp = len(hist_jog)
   
   t = 1
   while t <= comp:
-    aux_df['Atleta'][t] = hist_jog['DESCRIÇÃO HISTÓRICO'][t-1]
-  
-  
+    aux_df.loc[len(aux_df)] = hist_jog['DESCRIÇÃO HISTÓRICO'][t-1]
+
   df_hist = pd.concat([df_hist,aux_df])
 
 st.write(df_hist)  
