278d3d24cd70274f06a7d4c56ebd43f5d2abde1c
cru-saf-apps <116762584+cru-saf-apps@users.noreply.github.com>
2022-11-21 14:18:20 -0300

    Update Lista_Semanal_Mercado.py

--- pages/Lista_Semanal_Mercado.py
+++ pages/Lista_Semanal_Mercado.py
@@ -26,33 +26,33 @@ col11, col9, col7 = st.columns(3)
 
 with col11:
   st.subheader('Ext. Esquerdo')
-  st.write(base[base.Posição==11])
+  st.write(base[base.Posição==11].nlargest(10,'Nota')[['Nome','Clube','Data de Nascimento']])
 
 with col9:
   st.subheader('Centroavante')
-  st.write(base[base.Posição==9])
+  st.write(base[base.Posição==9].nlargest(10,'Nota')[['Nome','Clube','Data de Nascimento']])
 
 with col7:
   st.subheader('Ext. Direito')
-  st.write(base[base.Posição==7])
+  st.write(base[base.Posição==7].nlargest(10,'Nota')[['Nome','Clube','Data de Nascimento']])
 
   
 col10, col9meio = st.columns(2)
 
 with col10:
   st.subheader('Meia')
-  st.write(base[base.Posição == 10])
+  st.write(base[base.Posição == 10].nlargest(10,'Nota')[['Nome','Clube','Data de Nascimento']])
   
 with col9meio:
   st.subheader('Segundo atacante')
-  st.write(base[base.Posição==9.5])
+  st.write(base[base.Posição==9.5].nlargest(10,'Nota')[['Nome','Clube','Data de Nascimento']])
   
   
 col8meio, col5, col8 = st.columns(3)
 
 with col8meio:
   st.subheader('Médio Ofensivo')
-  st.write(base[base.Posição == 8.5])
+  st.write(base[base.Posição == 8.5].nlargest(10,'Nota')[['Nome','Clube','Data de Nascimento']])
   
 with col5:
   st.subheader('Médio Defensivo')
@@ -62,6 +62,23 @@ with col8:
   st.subheader('Box to Box')
   st.write(base[base.Posição == 8].nlargest(10,'Nota')[['Nome','Clube','Data de Nascimento']])
 
+col6, col3, col4, col2 = st.columns(4)
 
+with col6:
+  st.subheader('Box to Box')
+  st.write(base[base.Posição == 6].nlargest(10,'Nota')[['Nome','Clube','Data de Nascimento']])
+
+with col3:
+  st.subheader('Box to Box')
+  st.write(base[base.Posição == 3].nlargest(10,'Nota')[['Nome','Clube','Data de Nascimento']])
+  
+with col4:
+  st.subheader('Box to Box')
+  st.write(base[base.Posição == 4].nlargest(10,'Nota')[['Nome','Clube','Data de Nascimento']])
+  
+with col2:
+  st.subheader('Box to Box')
+  st.write(base[base.Posição == 2].nlargest(10,'Nota')[['Nome','Clube','Data de Nascimento']])
+  
   
   
