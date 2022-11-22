8321b12181ba30cfc756429a63caa437a9ad54d4
cru-saf-apps <116762584+cru-saf-apps@users.noreply.github.com>
2022-11-22 14:02:23 -0300

    Update teste_google_st.py

--- pages/teste_google_st.py
+++ pages/teste_google_st.py
@@ -3,6 +3,12 @@ from google.oauth2 import service_account
 from gsheetsdb import connect
 import pandas as pd
 
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
 # Create a connection object.
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"],
@@ -14,11 +20,7 @@ conn = connect(credentials=credentials)
 
 # Perform SQL query on the Google Sheet.
 # Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    rows = rows.fetchall()
-    return rows
+
 
 sheet_url = st.secrets["private_gsheets_url"].private_gsheets_url
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
