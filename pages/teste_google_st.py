30a9aa02222c3c16baddd555a2d55cbc5b9175dc
cru-saf-apps <116762584+cru-saf-apps@users.noreply.github.com>
2022-11-22 13:26:17 -0300

    Update teste_google_st.py

--- pages/teste_google_st.py
+++ pages/teste_google_st.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from google.oauth2 import service_account
-from ggsheets import connect
+from gsheets import connect
 
 # Create a connection object.
 credentials = service_account.Credentials.from_service_account_info(
