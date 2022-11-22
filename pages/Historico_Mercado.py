61bb3dd994b08603edd27b080a12c3d8f4449315
cru-saf-apps <116762584+cru-saf-apps@users.noreply.github.com>
2022-11-22 09:05:25 -0300

    Update Historico_Mercado.py

--- pages/Historico_Mercado.py
+++ pages/Historico_Mercado.py
@@ -1,7 +1,13 @@
 import streamlit as st
 import pandas as pd
 import openpyxl
+from fpdf import FPDF
+import base64
 
+
+def create_download_link(val, filename):
+    b64 = base64.b64encode(val)  # val looks like b'...'
+    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
 
 
 negoc = pd.read_excel('NEGOCIAÇÕES.xlsx',engine='openpyxl')
@@ -26,5 +32,14 @@ for jogador in negoc.ID:
 
     t+=1
 
+export_as_pdf = st.button("Export Report")
 
+if export_as_pdf:
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font('Arial', 'B', 16)
+    pdf.cell(40, 10, report_text)
+    
+    html = create_download_link(pdf.output(dest="S").encode("latin-1"), "test")
 
+    st.markdown(html, unsafe_allow_html=True)
