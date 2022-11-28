b178c8dbe1223cdda54dc2ef86ed7052df01efe7
cru-saf-apps <116762584+cru-saf-apps@users.noreply.github.com>
2022-11-28 11:38:45 -0300

    Update Lista_Semanal_Mercado.py

--- pages/Lista_Semanal_Mercado.py
+++ pages/Lista_Semanal_Mercado.py
@@ -1,11 +1,31 @@
 import streamlit as st
 import pandas as pd
-import openpyxl
+from gspread_pandas import Spread,Client
+from google.oauth2 import service_account
 
 st.set_page_config(layout="wide")
 
+def load_spreadsheet(spreadsheet_name):
+    worksheet = sh.worksheet(spreadsheet_name)
+    df = pd.DataFrame(worksheet.get_all_records())
+    return df
+  
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
 
-base = pd.read_excel('DESTAQUES.xlsx',engine='openpyxl')
+
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=scope
+)
+
+client = Client(scope = scope, creds = credentials)
+
+spreadsheet_name = "DESTAQUES"
+spread = Spread(spreadsheet_name, client = client)
+sh = client.open(spreadsheet_name)
+base = load_spreadsheet(spreadsheet_name)
+
 
 dic_classe = {'A':6,
               'B':5,
