8d7722ef3598d9dc944234fdb7b2a3f7ce5c2738
cru-saf-apps <116762584+cru-saf-apps@users.noreply.github.com>
2022-12-05 10:23:23 -0300

    Create mudancadata.py

---
+++ pages/mudancadata.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd
+from gspread_pandas import Spread,Client
+from google.oauth2 import service_account
+import time
+import datetime as dt
+
+
+def load_spreadsheet(spreadsheet_name):
+    worksheet = sh.worksheet(spreadsheet_name)
+    df = pd.DataFrame(worksheet.get_all_records())
+    return df
+
+def update_spreadsheet(spreadsheet_name, df):
+    spread.df_to_sheet(df,sheet = spreadsheet_name,index=False,replace=True)
+    
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=scope
+)
+
+client = Client(scope = scope, creds = credentials)
+
+
+
+
+spreadsheet_name = "BASE ELENCOS"
+spread = Spread(spreadsheet_name, client = client)
+sh = client.open(spreadsheet_name)
+base_elencos = load_spreadsheet(spreadsheet_name)
+
+
+base_elencos['Data Nascimento'] = base_elencos['Data Nascimento'].to_datetime(errors='ignore',format='%d/%m/%Y')
+
+
+update_spreadsheet(spreadsheet_name,base_elencos)
